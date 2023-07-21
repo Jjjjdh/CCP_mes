02033970e6a329a7131964c10bdcb36939996e99
Administrator <Administrator@DESKTOP-KATFP17>
2023-07-21 17:50:15 +0900

    20230721_1750

--- MainForms/P03_MainForm.Designer.cs
+++ MainForms/P03_MainForm.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.components = new System.ComponentModel.Container();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnExit = new System.Windows.Forms.ToolStripButton();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.stsFormName = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
@@ -37,18 +38,17 @@
             this.stsNowdate = new System.Windows.Forms.ToolStripStatusLabel();
             this.timNowDate = new System.Windows.Forms.Timer(this.components);
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.btnMaterial = new System.Windows.Forms.Button();
             this.btnProduct = new System.Windows.Forms.Button();
             this.btnProductControl = new System.Windows.Forms.Button();
             this.btnProductMoniter = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.btnExit = new System.Windows.Forms.ToolStripButton();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
             this.toolStrip1.SuspendLayout();
             this.statusStrip1.SuspendLayout();
             this.flowLayoutPanel1.SuspendLayout();
@@ -66,6 +66,24 @@
             this.toolStrip1.Size = new System.Drawing.Size(939, 116);
             this.toolStrip1.TabIndex = 1;
             this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.btnExit.AutoSize = false;
+            this.btnExit.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnExit.Font = new System.Drawing.Font("맑은 고딕", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnExit.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnExit.Image = global::MainForms.Properties.Resources.btn종료;
+            this.btnExit.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.btnExit.ImageTransparentColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(100, 100);
+            this.btnExit.Text = "종료";
+            this.btnExit.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnExit.TextDirection = System.Windows.Forms.ToolStripTextDirection.Horizontal;
+            this.btnExit.TextImageRelation = System.Windows.Forms.TextImageRelation.Overlay;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // statusStrip1
             // 
@@ -130,6 +148,33 @@
             this.flowLayoutPanel1.Size = new System.Drawing.Size(334, 709);
             this.flowLayoutPanel1.TabIndex = 3;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Image = global::MainForms.Properties.Resources.Logo;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(326, 113);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.button1.Image = global::MainForms.Properties.Resources.pngegg;
+            this.button1.Location = new System.Drawing.Point(3, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(326, 70);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "●  생산관리";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
             // btnMaterial
             // 
             this.btnMaterial.BackColor = System.Drawing.Color.AliceBlue;
@@ -188,6 +233,21 @@
             this.btnProductMoniter.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnProductMoniter.UseVisualStyleBackColor = false;
             // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button6.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.button6.Image = global::MainForms.Properties.Resources.pngegg;
+            this.button6.Location = new System.Drawing.Point(3, 406);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(326, 70);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "●  CCP 관리";
+            this.button6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button6.UseVisualStyleBackColor = false;
+            // 
             // button7
             // 
             this.button7.BackColor = System.Drawing.Color.AliceBlue;
@@ -240,66 +300,6 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(939, 593);
             this.panel1.TabIndex = 4;
-            // 
-            // btnExit
-            // 
-            this.btnExit.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
-            this.btnExit.AutoSize = false;
-            this.btnExit.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnExit.Font = new System.Drawing.Font("맑은 고딕", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.btnExit.ForeColor = System.Drawing.Color.DeepSkyBlue;
-            this.btnExit.Image = global::MainForms.Properties.Resources.btn종료;
-            this.btnExit.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
-            this.btnExit.ImageTransparentColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(100, 100);
-            this.btnExit.Text = "종료";
-            this.btnExit.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.btnExit.TextDirection = System.Windows.Forms.ToolStripTextDirection.Horizontal;
-            this.btnExit.TextImageRelation = System.Windows.Forms.TextImageRelation.Overlay;
-            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pictureBox1.Image = global::MainForms.Properties.Resources.Logo;
-            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(326, 113);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.LightSteelBlue;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.button1.ForeColor = System.Drawing.Color.RoyalBlue;
-            this.button1.Image = global::MainForms.Properties.Resources.pngegg;
-            this.button1.Location = new System.Drawing.Point(3, 122);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(326, 70);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "●  생산관리";
-            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.UseVisualStyleBackColor = false;
-            // 
-            // button6
-            // 
-            this.button6.BackColor = System.Drawing.Color.LightSteelBlue;
-            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button6.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.button6.ForeColor = System.Drawing.Color.RoyalBlue;
-            this.button6.Image = global::MainForms.Properties.Resources.pngegg;
-            this.button6.Location = new System.Drawing.Point(3, 406);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(326, 70);
-            this.button6.TabIndex = 9;
-            this.button6.Text = "●  CCP 관리";
-            this.button6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button6.UseVisualStyleBackColor = false;
             // 
             // P03_MainForm
             // 
